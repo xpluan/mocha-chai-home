6948bf3c9d3e8fc781cff6f900cb3963e7db8e7a
xpluan <92053296+xpluan@users.noreply.github.com>
2020-10-24 13:57:51 +0000

    fixes #25

--- src/NetworkSecurity/NetworkSecurity/Helper/CryptographyHelper.cs
+++ src/NetworkSecurity/NetworkSecurity/Helper/CryptographyHelper.cs
@@ -14,4 +14,6 @@ namespace NetworkSecurity.Helper
                 case EnumCryptographyAlgorithms.Aes: return new AesCryptoServiceProvider();// :AES  :SymmetricAlgorithm
                 case EnumCryptographyAlgorithms.Des: return new DESCryptoServiceProvider();// :DES :SymmetricAlgorithm
                 case EnumCryptographyAlgorithms.TripleDes: return new TripleDESCryptoServiceProvider(); // :TripleDES :SymmetricAlgorithm
-                case EnumCryptographyAlgorithms.Rc2: return new RC2CryptoServiceProvider();// :RC2 :S
+                case EnumCryptographyAlgorithms.Rc2: return new RC2CryptoServiceProvider();// :RC2 :SymmetricAlgorithm
+
+                // Ha
