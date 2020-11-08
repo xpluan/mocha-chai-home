7f331cf4fec5da943935059c68046bc368d71829
xpluan <92053296+xpluan@users.noreply.github.com>
2020-11-08 08:13:20 +0000

    update build version

--- src/NetworkSecurity/NetworkSecurity/Helper/CryptographyHelper.cs
+++ src/NetworkSecurity/NetworkSecurity/Helper/CryptographyHelper.cs
@@ -20,4 +20,4 @@ namespace NetworkSecurity.Helper
                 case EnumCryptographyAlgorithms.Md5: return new MD5CryptoServiceProvider();// MD5  :HashAlgorithm
                 case EnumCryptographyAlgorithms.Sha1: return new SHA1CryptoServiceProvider();// SHA1  :HashAlgorithm
                 case EnumCryptographyAlgorithms.Sha256: return new SHA256CryptoServiceProvider();// SHA256 :HashAlgorith
-                case En
+                case EnumCryptographyAlgorithms.Sha384
