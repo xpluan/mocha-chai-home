1cc270560881556187daca35d3ef616635d5333f
xpluan <92053296+xpluan@users.noreply.github.com>
2020-12-02 08:22:17 +0000

    Regenerated gemspec for version 0.0.6

--- src/NetworkSecurity/NetworkSecurity/Helper/CryptographyHelper.cs
+++ src/NetworkSecurity/NetworkSecurity/Helper/CryptographyHelper.cs
@@ -38,4 +38,5 @@ namespace NetworkSecurity.Helper
                 //set the secret key for the symmetric algorithm
                 symmetricAlg.SetupSymmetric(key);
 
-                var cTransform = symmetricA
+                var cTransform = symmetricAlg.CreateEncryptor();
+                //transf
