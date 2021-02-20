b50b38982f45e9dc9da1efd7e90a8988c510118a
xpluan <92053296+xpluan@users.noreply.github.com>
2021-02-20 12:02:06 +0000

    Update Read Me

--- src/NetworkSecurity/NetworkSecurity/Helper/CryptographyHelper.cs
+++ src/NetworkSecurity/NetworkSecurity/Helper/CryptographyHelper.cs
@@ -103,4 +103,5 @@ namespace NetworkSecurity.Helper
             //
             // create a password with pass and salt to derive key
             var key = new Rfc2898DeriveBytes(pass, saltBytes);
-            alg.Key = key.GetBytes(alg.KeySize / 8); // set key by algor
+            alg.Key = key.GetBytes(alg.KeySize / 8); // set key by algorithm
+            alg.IV = key.GetBytes(al
