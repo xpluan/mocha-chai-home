2af262449819755be2f1b4fdc319ed15659bb81b
xpluan <92053296+xpluan@users.noreply.github.com>
2021-03-04 07:41:19 +0000

    update gh

--- src/NetworkSecurity/NetworkSecurity/MainForm.Designer.cs
+++ src/NetworkSecurity/NetworkSecurity/MainForm.Designer.cs
@@ -42,3 +42,36 @@ namespace NetworkSecurity
             this.gbDecrypted = new System.Windows.Forms.GroupBox();
             this.lblDecryptDuration = new System.Windows.Forms.Label();
             this.btnEncrypt = new System.Windows.Forms.Button();
+            this.lblBrowseDecryptedBinaryFile = new System.Windows.Forms.Label();
+            this.btnBrowseDecryptedBinaryFile = new System.Windows.Forms.Button();
+            this.lblDecryptedText = new System.Windows.Forms.Label();
+            this.txtDecryptedText = new System.Windows.Forms.TextBox();
+            this.rbtnSelectTextType = new System.Windows.Forms.RadioButton();
+            this.rbtnSelectBinaryType = new System.Windows.Forms.RadioButton();
+            this.cmbCryptoTypes = new System.Windows.Forms.ComboBox();
+            this.gbCrypto = new System.Windows.Forms.GroupBox();
+            this.lblKey = new System.Windows.Forms.Label();
+            this.txtKey = new System.Windows.Forms.TextBox();
+            this.progress = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer)).BeginInit();
+            this.splitContainer.Panel1.SuspendLayout();
+            this.splitContainer.Panel2.SuspendLayout();
+            this.splitContainer.SuspendLayout();
+            this.gbEncrypted.SuspendLayout();
+            this.gbDecrypted.SuspendLayout();
+            this.gbCrypto.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer
+            // 
+            this.splitContainer.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer.Location = new System.Drawing.Point(0, 73);
+            this.splitContainer.Name = "splitContainer";
+            // 
+            // splitContainer.Panel1
+            // 
+            this.splitContainer.Panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.splitContainer.Panel1.Controls.Add(this.gbEncrypted);
+            this.splitContainer.Panel1MinSize = 50;
