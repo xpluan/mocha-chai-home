3be15dcdf269fd00b820d4ff524f5346efbe0b08
xpluan <92053296+xpluan@users.noreply.github.com>
2021-03-14 09:58:58 +0000

    Upgraded to the latest conformance suite

--- src/NetworkSecurity/NetworkSecurity/MainForm.Designer.cs
+++ src/NetworkSecurity/NetworkSecurity/MainForm.Designer.cs
@@ -219,3 +219,39 @@ namespace NetworkSecurity
             this.btnBrowseDecryptedBinaryFile.Size = new System.Drawing.Size(434, 43);
             this.btnBrowseDecryptedBinaryFile.TabIndex = 10;
             this.btnBrowseDecryptedBinaryFile.Text = "Browse Binary File";
+            this.btnBrowseDecryptedBinaryFile.UseVisualStyleBackColor = true;
+            // 
+            // lblDecryptedText
+            // 
+            this.lblDecryptedText.AutoSize = true;
+            this.lblDecryptedText.Location = new System.Drawing.Point(33, 45);
+            this.lblDecryptedText.Name = "lblDecryptedText";
+            this.lblDecryptedText.Size = new System.Drawing.Size(108, 17);
+            this.lblDecryptedText.TabIndex = 9;
+            this.lblDecryptedText.Text = "Decrypted Text:";
+            // 
+            // txtDecryptedText
+            // 
+            this.txtDecryptedText.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtDecryptedText.Location = new System.Drawing.Point(36, 65);
+            this.txtDecryptedText.Multiline = true;
+            this.txtDecryptedText.Name = "txtDecryptedText";
+            this.txtDecryptedText.Size = new System.Drawing.Size(434, 223);
+            this.txtDecryptedText.TabIndex = 8;
+            // 
+            // rbtnSelectTextType
+            // 
+            this.rbtnSelectTextType.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.rbtnSelectTextType.AutoSize = true;
+            this.rbtnSelectTextType.Checked = true;
+            this.rbtnSelectTextType.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.rbtnSelectTextType.Location = new System.Drawing.Point(826, 27);
+            this.rbtnSelectTextType.Name = "rbtnSelectTextType";
+            this.rbtnSelectTextType.Size = new System.Drawing.Size(56, 21);
+            this.rbtnSelectTextType.TabIndex = 6;
+            this.rbtnSelectTextType.TabStop = true;
+            this.rbtnSelectTextType.Text = "Text";
+            this.rbtnSelectTextType.UseVisualStyleBackColor = true;
+            // 
