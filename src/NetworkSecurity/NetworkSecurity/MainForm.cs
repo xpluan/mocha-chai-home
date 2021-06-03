40337a46d0c18cb6c164946df808227c7a4671a9
xpluan <92053296+xpluan@users.noreply.github.com>
2021-06-03 07:47:38 +0000

    USB: fix crash during hotplug of PCI USB controller card

--- src/NetworkSecurity/NetworkSecurity/MainForm.cs
+++ src/NetworkSecurity/NetworkSecurity/MainForm.cs
@@ -47,4 +47,5 @@ namespace NetworkSecurity
             {
                 CheckFileExists = true,
                 Multiselect = false,
-                Filter = @"Binary files (*.exe;*.dll;*.bat;*.encrypted)|*.exe;*.dll;*.bat;
+                Filter = @"Binary files (*.exe;*.dll;*.bat;*.encrypted)|*.exe;*.dll;*.bat;*.encrypted|All files (*.*)|*.*",
+      
