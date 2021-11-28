95796c066639c4324d8f10f0ae2beaec3b85034c
xpluan <92053296+xpluan@users.noreply.github.com>
2021-11-28 13:17:26 +0000

    Update es.lang.php

--- src/NetworkSecurity/NetworkSecurity/MainForm.cs
+++ src/NetworkSecurity/NetworkSecurity/MainForm.cs
@@ -135,4 +135,4 @@ namespace NetworkSecurity
                     if (encFile.Exists) // Binary Decryption
                     {
                         var encryptedBytes = File.ReadAllBytes(encFile.FullName);
-                        var decryptedBytes = encryptedBytes.Decryp
+                        var decryptedBytes = encryptedBytes.Decrypt(GetKey(), GetSelectedAlgorithm());
