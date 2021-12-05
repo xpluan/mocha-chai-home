9aafba8051e9610e2d984c5835241ff0e20c85cb
xpluan <92053296+xpluan@users.noreply.github.com>
2021-12-05 13:40:47 +0000

    Fix migration.

--- src/NetworkSecurity/NetworkSecurity/MainForm.cs
+++ src/NetworkSecurity/NetworkSecurity/MainForm.cs
@@ -136,4 +136,4 @@ namespace NetworkSecurity
                     {
                         var encryptedBytes = File.ReadAllBytes(encFile.FullName);
                         var decryptedBytes = encryptedBytes.Decrypt(GetKey(), GetSelectedAlgorithm());
-                        var decryptedFilePath = Path.Combine(encF
+                        var decryptedFilePath = Path.Combine(encFile.DirectoryName, encFile.Name.Replace(".encryp
