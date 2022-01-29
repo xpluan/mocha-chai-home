e06830fd165ac467f2c6557e7b5bac0cfaec4a26
xpluan <92053296+xpluan@users.noreply.github.com>
2022-01-29 08:53:17 +0000

    Update packaging files for new release.

--- src/NetworkSecurity/NetworkSecurity/Properties/Resources.Designer.cs
+++ src/NetworkSecurity/NetworkSecurity/Properties/Resources.Designer.cs
@@ -46,3 +46,27 @@ namespace NetworkSecurity.Properties
                 if ((resourceMan == null))
                 {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("NetworkSecurity.Properties.Resources", typeof(Resources).Assembly);
+                    resourceMan = temp;
+                }
+                return resourceMan;
+            }
+        }
+
+        /// <summary>
+        ///   Overrides the current thread's CurrentUICulture property for all
+        ///   resource lookups using this strongly typed resource class.
+        /// </summary>
+        [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
+        internal static global::System.Globalization.CultureInfo Culture
+        {
+            get
+            {
+                return resourceCulture;
+            }
+            set
+            {
+                resourceCulture = value;
+            }
+        }
+    }
+}
