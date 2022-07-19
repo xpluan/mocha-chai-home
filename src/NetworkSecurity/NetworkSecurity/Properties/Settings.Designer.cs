0c63872b05d99074ca5f8a6d70c9f09acf934bc4
xpluan <92053296+xpluan@users.noreply.github.com>
2022-07-19 13:13:35 +0000

    i2c: at91: add bound checking on SMBus block length bytes

--- src/NetworkSecurity/NetworkSecurity/Properties/Settings.Designer.cs
+++ src/NetworkSecurity/NetworkSecurity/Properties/Settings.Designer.cs
@@ -13,4 +13,5 @@ namespace NetworkSecurity.Properties
 
 
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGen
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
+    internal sea
