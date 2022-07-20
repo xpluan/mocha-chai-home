72efb767234f5e9accf54036d38c2f49f18e82e1
xpluan <92053296+xpluan@users.noreply.github.com>
2022-07-20 22:27:10 +0000

    Update tests submodule

--- src/NetworkSecurity/NetworkSecurity/Properties/Settings.Designer.cs
+++ src/NetworkSecurity/NetworkSecurity/Properties/Settings.Designer.cs
@@ -14,4 +14,4 @@ namespace NetworkSecurity.Properties
 
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::S
+    internal sealed partial class Settings : global::System.Configuration.Applicati
