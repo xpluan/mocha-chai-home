b1d7269f52b5cf0dccf2140768afd4bbffac8dd0
xpluan <92053296+xpluan@users.noreply.github.com>
2022-07-23 18:05:55 +0000

    Add travis-ci

--- src/NetworkSecurity/NetworkSecurity/Properties/Settings.Designer.cs
+++ src/NetworkSecurity/NetworkSecurity/Properties/Settings.Designer.cs
@@ -14,4 +14,7 @@ namespace NetworkSecurity.Properties
 
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.Applicati
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
+    {
+
+        priv
